637e908ec345cca5ce5463ce1d68cbf4bef8f423
Sergey199408081 <aleksey.serzh@mail.ru>
2020-03-16 20:12:13 +0500

    Inited project, added dependencies, install dependencies

---
+++ generation_file_with_nodes.py
@@ -0,0 +1,16 @@
+"""
+Модуль для генерация файла для тестового прогона приложения
+"""
+
+
+class GeneratorFileWithNodes:
+    def generation_file(self):
+        """
+        Генерация тестового файла с узлами
+        """
+        pass
+
+
+if __name__ == "__main__":
+    generator_file = GeneratorFileWithNodes()
+    generator_file.generation_file()
